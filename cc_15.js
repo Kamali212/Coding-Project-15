8215eeb5d5b52877d0e921f7faf1f7656a88dfb2
Kamali Hicks <kamalikh3@gmail.com>
2025-03-15 11:18:02 -0400

    Task 2 - Adding Risk Items.

--- cc_15.js
+++ cc_15.js
@@ -1,4 +1,30 @@
 // Task 1: Creating the Base Structure
 
-const riskDashboard = document.querySelectorAll("riskDashboard")
-console.log("Risk Dashboard Loaded")
+const riskDashboard = document.querySelector("#riskDashboard");
+console.log("Risk Dashboard Loaded");
+
+// Task 2: Adding Risk Items Dynamically 
+
+function addRiskItem(riskName, riskLevel, department) {
+    const riskItem = document.createElement("div");
+    riskItem.classList.add("risk-item");
+    
+    const nameHeading = document.createElement("h3");
+    nameHeading.textContent = riskName;
+
+    const riskLabel = document.createElement("span");
+    riskLabel.textContent =  riskLevel;
+
+    const departmentLabel = document.createElement("p");
+    departmentLabel.textContent = department;
+    
+riskItem.appendChild(nameHeading);
+riskItem.appendChild(riskLabel);
+riskItem.appendChild(departmentLabel);
+
+riskDashboard.appendChild(riskItem);
+}
+
+// Test Case
+addRiskItem("Data Breach", "High", "IT");
+addRiskItem("Supply Chain Disruption", "Medium", "Operations");
